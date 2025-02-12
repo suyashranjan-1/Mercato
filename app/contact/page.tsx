e6a7d4d2aee706476a86785310459b304fe94fa8
Siddharth-Nama <siddharthnama2604@gmail.com>
2025-02-12 16:19:27 +0530

    contact form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useRef } from "react";
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { useForm } from "react-hook-form";
@@ -66,7 +66,11 @@ type FormValues = {
   email: string;
   job_title: string;
   company_name: string;
-  help: "Evaluate Mercato Agency for my company" | "Learn More" | "Get a Quote" | "Other";
+  help:
+    | "Evaluate Mercato Agency for my company"
+    | "Learn More"
+    | "Get a Quote"
+    | "Other";
   services:
     | "SEO"
     | "Video Editing"
@@ -77,8 +81,6 @@ type FormValues = {
   terms: boolean;
 };
 
-
-
 export default function ContactForm() {
   const [loading, setLoading] = useState(false);
   const [submitted, setSubmitted] = useState(false);
@@ -88,16 +90,18 @@ export default function ContactForm() {
     e.preventDefault();
     if (!form.current) return;
     emailjs
-      .sendForm('service_57dccsx', 'template_4ovd2wl', form.current, {
-        publicKey: '5_BKNco2OULcaqW0m',
+      .sendForm("service_57dccsx", "template_4ovd2wl", form.current, {
+        publicKey: "5_BKNco2OULcaqW0m",
       })
       .then(
         () => {
-          alert('We have received your inquiry and will be contacting you via email shortly.');
-          console.log('SUCCESS!');
+          alert(
+            "We have received your inquiry and will be contacting you via email shortly."
+          );
+          console.log("SUCCESS!");
         },
         (error) => {
-          console.log('FAILED...', error.text);
+          console.log("FAILED...", error.text);
         }
       );
   };
@@ -126,134 +130,185 @@ export default function ContactForm() {
               text-gray-300
                     "
           >
-            Let&apos;s talk about how Mercato Agency can help your team work better.
+            Let&apos;s talk about how Mercato Agency can help your team work
+            better.
           </div>
 
           <div className="bg-[#f6f5f4] md:w-11/12  space-y-6 p-4 rounded-2xl my-4 hidden md:flex md:flex-col">
             <div className="flex gap-4 border-b ">
               <div className=" font-normal pb-4 ">
-              One flexible agency for your entire company to optimize SEO, create stunning videos, run high-performing Google & Meta ads, design eye-catching thumbnails, and craft impactful graphics.
+                One flexible agency for your entire company to optimize SEO,
+                create stunning videos, run high-performing Google & Meta ads,
+                design eye-catching thumbnails, and craft impactful graphics.
               </div>
             </div>
 
             <div className="flex gap-4 border-b ">
               <div className=" font-normal pb-4 ">
-              Enterprise features to securely manage campaigns, analytics, and creative assets.
+                Enterprise features to securely manage campaigns, analytics, and
+                creative assets.
               </div>
             </div>
 
             <div className="flex gap-4  ">
               <div className=" font-normal pb-4 ">
-              Dedicated support to work with you on your strategy and help you build the best plan for your business growth.
+                Dedicated support to work with you on your strategy and help you
+                build the best plan for your business growth.
               </div>
             </div>
           </div>
         </div>
 
-          <form ref={form} onSubmit={sendEmail} className=" space-y-3 h-full border rounded-3xl p-10 md:w-2/3">
-              
-                <div className=" space-y-3 items-center justify-center w-full">
-                  <label className="font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-sm bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">Full name *</label><br />
-                  <input type="text" required name="first_name" className="form-control, flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50" />
-                </div>
-                
-                <div className=" space-y-3 items-center justify-center w-full">
-                  <label className="text-sm bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">Email *</label>
-                  <input type="email" required name="email" className="form-control, flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50" />
-                </div>
-                <div className="  space-y-3 items-center justify-center w-full">
-                  <label className="text-sm bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">Company name? (if any)</label>
-                  <input type="text" name="company_name" className="form-control, flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50" />
-                </div>
-                <div className=" space-y-3 items-center justify-center w-full">
-                  <label className="text-sm bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">Services you are interested in</label>
-                  <Select name="services" required>
-                    <SelectTrigger>
-                      <SelectValue placeholder="Select an option" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectItem value="SEO">SEO</SelectItem>
-                      <SelectItem value="Ads">Ads</SelectItem>
-                      <SelectItem value="Thumbnail Creation">Thumbnail Creation</SelectItem>
-                      <SelectItem value="Graphic Design">Graphic Design</SelectItem>
-                      <SelectItem value="Video Editing">Video Editing</SelectItem>
-                    </SelectContent>
-                  </Select>
-                </div>
-                <div className=" space-y-3 items-center justify-center w-full">
-                  <label className="text-sm bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">How can we help?</label>
-                  <Select name="help" required>
-                    <SelectTrigger>
-                      <SelectValue placeholder="Select an option" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectItem value="Evaluate Mercato Agency for my company">Evaluate Mercato Agency <br/>for my company</SelectItem>
-                      <SelectItem value="Learn More">Learn More</SelectItem>
-                      <SelectItem value="Get a Quote">Get a Quote</SelectItem>
-                      <SelectItem value="Other">Other</SelectItem>
-                    </SelectContent>
-                  </Select>
-                </div>
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className=" space-y-3 h-full border rounded-3xl p-10 md:w-2/3"
+        >
+          <div className=" space-y-3 items-center justify-center w-full">
+            <label className="font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-sm bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+              Full name *
+            </label>
+            <br />
+            <input
+              type="text"
+              required
+              name="first_name"
+              className="form-control, flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+            />
+          </div>
 
-                <div className="space-y-3 items-center justify-center w-full">
-  <label className="text-sm bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-    Preferred date for an online meet or call!
-  </label>
-  <div className="flex space-x-2 w-full">
-    <Input
-      type="date"
-      name="last_name"
-      min={new Date().toISOString().split('T')[0]} // This sets minimum date to today
-      className="w-1/2"
-      required
-    />
-    <Select name="last_name" required>
-      <SelectTrigger>
-        <SelectValue placeholder="Select time slot" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectItem value="9:00 AM - 11:00 AM">
-          9:00 AM - 11:00 AM (UTC-5:00)
-        </SelectItem>
-        <SelectItem value="11:00 AM - 1:00 PM">
-          11:00 AM - 1:00 PM (UTC-5:00)
-        </SelectItem>
-        <SelectItem value="2:00 PM - 4:00 PM">
-          2:00 PM - 4:00 PM (UTC-5:00)
-        </SelectItem>
-        <SelectItem value="4:00 PM - 6:00 PM">
-          4:00 PM - 6:00 PM (UTC-5:00)
-        </SelectItem>
-        <SelectItem value="6:00 PM - 8:00 PM">
-          6:00 PM - 8:00 PM (UTC-5:00)
-        </SelectItem>
-      </SelectContent>
-    </Select>
-  </div>
-</div>
+          <div className=" space-y-3 items-center justify-center w-full">
+            <label className="text-sm bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+              Email *
+            </label>
+            <input
+              type="email"
+              required
+              name="email"
+              className="form-control, flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+            />
+          </div>
+          <div className="  space-y-3 items-center justify-center w-full">
+            <label className="text-sm bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+              Company name? (if any)
+            </label>
+            <input
+              type="text"
+              name="company_name"
+              className="form-control, flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+            />
+          </div>
+          <div className=" space-y-3 items-center justify-center w-full">
+            <label className="text-sm bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+              Services you are interested in
+            </label>
+            <Select name="services" required>
+              <SelectTrigger>
+                <SelectValue placeholder="Select an option" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="SEO">SEO</SelectItem>
+                <SelectItem value="Ads">Ads</SelectItem>
+                <SelectItem value="Thumbnail Creation">
+                  Thumbnail Creation
+                </SelectItem>
+                <SelectItem value="Graphic Design">Graphic Design</SelectItem>
+                <SelectItem value="Video Editing">Video Editing</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          <div className=" space-y-3 items-center justify-center w-full">
+            <label className="text-sm bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+              How can we help?
+            </label>
+            <Select name="help" required>
+              <SelectTrigger>
+                <SelectValue placeholder="Select an option" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="Evaluate Mercato Agency for my company">
+                  Evaluate Mercato Agency <br />
+                  for my company
+                </SelectItem>
+                <SelectItem value="Learn More">Learn More</SelectItem>
+                <SelectItem value="Get a Quote">Get a Quote</SelectItem>
+                <SelectItem value="Other">Other</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
 
-                <div className=" space-y-3 items-center justify-center w-full">
-                  <label className="text-sm bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">Anything else?</label>
-                  <textarea name="info" className="form-control, flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"></textarea>
-                </div>
-                <div className="form-check, flex gap-4 items-center">
-                  <input required type="checkbox" name="terms" className="form-check-input, peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground, outline
+          <div className="space-y-3 items-center justify-center w-full">
+            <label className="text-sm bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+              Preferred date for an online meet or call!
+            </label>
+            <div className="flex space-x-2 w-full">
+              <Input
+              type="date"
+              name="last_name"
+              min={new Date().toISOString().split("T")[0]}
+              className="w-1/2 uppercase" // Added uppercase class
+              required
+              />
+              <Select name="last_name" required>
+              <SelectTrigger>
+                <SelectValue placeholder="Select time slot" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="9:00 AM - 11:00 AM">
+                9:00 AM - 11:00 AM (UTC-5:00)
+                </SelectItem>
+                <SelectItem value="11:00 AM - 1:00 PM">
+                11:00 AM - 1:00 PM (UTC-5:00)
+                </SelectItem>
+                <SelectItem value="2:00 PM - 4:00 PM">
+                2:00 PM - 4:00 PM (UTC-5:00)
+                </SelectItem>
+                <SelectItem value="4:00 PM - 6:00 PM">
+                4:00 PM - 6:00 PM (UTC-5:00)
+                </SelectItem>
+                <SelectItem value="6:00 PM - 8:00 PM">
+                6:00 PM - 8:00 PM (UTC-5:00)
+                </SelectItem>
+              </SelectContent>
+              </Select>
+            </div>
+          </div>
+
+          <div className=" space-y-3 items-center justify-center w-full">
+            <label className="text-sm bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+              Anything else?
+            </label>
+            <textarea
+              name="info"
+              className="form-control, flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+            ></textarea>
+          </div>
+          <div className="form-check, flex gap-4 items-center">
+            <input
+              required
+              type="checkbox"
+              name="terms"
+              className="form-check-input, peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground, outline
                 
                 text-sm
                 font-light
-                bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400"/>
-                  <label className="text-xs font-light  md:w-3/4 mb-1 bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
-                    I agree to Mercato Agency&apos;s sending marketing communications related to Mercato Agency
-                  </label>
-                </div>
-                <div className="flex items-center gap-4">
-                  <Button type="submit" className="btn btn-primary, text-sm font-light" disabled={loading}>
-                    Submit
-                  </Button>
-                </div>
-          </form>
-
-
+                bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400"
+            />
+            <label className="text-xs font-light  md:w-3/4 mb-1 bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
+              I agree to Mercato Agency&apos;s sending marketing communications
+              related to Mercato Agency
+            </label>
+          </div>
+          <div className="flex items-center gap-4">
+            <Button
+              type="submit"
+              className="btn btn-primary, text-sm font-light"
+              disabled={loading}
+            >
+              Submit
+            </Button>
+          </div>
+        </form>
       </div>
     </div>
   );
