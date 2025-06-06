0f50a7dfabaab061faf6ee21ea74a774ce34f165
Mukulraj Singh <mukulraj7248@gmail.com>
2025-06-06 11:34:17 +0530

    Working on configuration issue

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     distDir: "build",
+    output: 'standalone', // or remove this line if not needed
     images: {
         domains: ["images.unsplash.com", "flowbite.s3.amazonaws.com"],
+    },
+    experimental: {
+        // Remove any experimental features that might cause issues
     },
     // ... any other existing config
 };
